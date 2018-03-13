dcff60283ea39f4ff527ece78c9db1854a15ef1e
DESKTOP-SD2SUIT\Çağatay TOPAÇ <cagataytopac@outlok.com>
2018-03-13 18:39:30 +0300

    Simple Blog Files Added

---
+++ SimpleBlog/App_Start/RouteConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SimpleBlog
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute("Login", "login", new { controller="Auth",action="login"});
+            routes.MapRoute("Home", "", new { controller = "Posts", action = "Index" });
+        }
+    }
+}
